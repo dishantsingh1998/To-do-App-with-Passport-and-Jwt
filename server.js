ac02b2753bff2bd11cf0e11a3f121574eaaa0e17
Dishant Singh <65276766+dishantsingh1998@users.noreply.github.com>
2021-04-15 17:43:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const users = require("./routes/api/users");
 
@@ -38,6 +39,20 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+//static assets if in production
+
+if(process.env.NODE_ENV ==='production'){
+
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res) =>{
+
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+
+  })
+}
+
 
 const port = process.env.PORT || 5000;
 
